7be4e36b613e35738a87c4ab8f227dd780a7fe0c
zrill° <159747311+fcknghatezrill@users.noreply.github.com>
2025-07-09 04:09:28 +0700

    Update config.py

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -3,21 +3,22 @@ from dotenv import load_dotenv
 
 load_dotenv(".env")
 
-MAX_BOT = int(os.getenv("MAX_BOT", "100"))
+MAX_BOT = int(os.environ.get("MAX_BOT"))
 
-DEVS = list(map(int, os.getenv("DEVS", "6139972751").split()))
+DEVS = list(map(int, os.environ.get("DEVS", "7779651486").split()))
 
-API_ID = int(os.getenv("API_ID", "20607074"))
+API_ID = int(os.environ.get("API_ID", "21801044"))
 
-API_HASH = os.getenv("API_HASH", "f5639cb2a94b785afc60800f351ec802")
+API_HASH = os.environ.get("API_HASH", "f8b8c21ad946683f79a4814c743cffd1")
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7600571701:AAGNyYxTB5yTaGo1bfEkTc7CtatqHUOxCPk")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7771682570:AAEm3PGqrjXgg_bEnKQaD8B2jUdj98uL>")
 
-OWNER_ID = int(os.getenv("OWNER_ID", "6139972751"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "7779651486"))
 
-BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1002125842026 -1002053287763 -1002044997044 -1002022625433 -1002050846285 -1002400165299 -1002416419679 -1001473548283").split()))
+BLACKLIST_CHAT = list(map(int, os.environ.get("BLACKLIST_CHAT", "-1002125842026").split()))
 
-RMBG_API = os.getenv("RMBG_API", "MA2sUZ4HdAfBegL36HiG4BUG")
+RMBG_API = os.environ.get("RMBG_API", "uiuHHXvfXK8ADSUycDCHzFU3")
 
-MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://fizzpamell:fizzpamell@cluster0.9nmhi5m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", "-4628173231"))
+MONGO_URL = os.environ.get("mongodb+srv://crystaline028:C2OChFlMoB2NI9fo@cluster0.1yhtvoo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+
+LOGS_MAKER_UBOT = int(os.environ.get("LOGS_MAKER_UBOT", "-2745239674"))
